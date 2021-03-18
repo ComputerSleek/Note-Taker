f3edada81de7e8c6fb73693dffc036f17b245117
Trill Ville <computersleek@gmail.com>
2021-03-18 18:51:52 -0400

    code push

--- server.js
+++ server.js
@@ -7,13 +7,19 @@ const path = require('path');
 const app = express();
 
 // Initial port setup
-var PORT = process.env.PORT || 8080;
+const PORT =  process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // 2 routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./public/index.html")(app);
+require("./public/notes.html")(app);
 
-app.listen(PORT, () => console.log("Server is running on " + PORT));
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+  });
+
+app.listen(PORT, () => {
+    console.log("Server is running on " + PORT);
+});
