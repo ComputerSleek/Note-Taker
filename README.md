676666fa1014538d0bb4a4329d929bfffd6f6877
Trill Ville <computersleek@gmail.com>
2021-05-04 19:21:20 -0400

    added npm uuid & edited server.js

--- README.md
+++ README.md
@@ -16,3 +16,6 @@ AS A user, I want to be able to write and save notes
 I WANT to be able to delete notes I've written before
 
 SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+## Technologies
+Javascript, Express.js, HTML5, CSS, Bootstrap
--- server.js
+++ server.js
@@ -1,30 +1,97 @@
-// Dependencies
-const express = require('express');
+// // Dependencies
+// const express = require('express');
 
-const path = require('path');
+// const path = require('path');
 
-// creating 'express' server
+// // creating 'express' server
+// const app = express();
+
+// // Initial port setup
+// const PORT =  process.env.PORT || 8080;
+
+// const notes = require("./db/db.json")
+
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.set('view engine', 'ejs')
+
+// app.set('views', './views');
+
+// // 2 routes
+// // require("./public/index.html")(app);
+// // require("./public/notes.html")(app);
+
+// app.get("/", function(req, res) {
+//     res.render("index");
+//   });
+
+// app.get("/notes", function(req, res) {
+//   res.render("notes");
+// });
+
+// app.listen(PORT, () => {
+//     console.log("Server is running on " + PORT);
+// });
+
+
+
+
+
+
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+
+
 const app = express();
+var PORT = process.env.PORT || 8080;
 
-// Initial port setup
-const PORT =  process.env.PORT || 8080;
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('view engine', 'ejs')
+app.use(express.static("public"));
 
-// 2 routes
-// require("./public/index.html")(app);
-// require("./public/notes.html")(app);
-
-app.get("/", function(req, res) {
-    res.render("index");
-  });
-
-app.get("/notes", function(req, res) {
-  res.render("notes");
+//Setting routes for APIs
+//This gets notes saved and joins it in db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
 });
 
-app.listen(PORT, () => {
-    console.log("Server is running on " + PORT);
+// Post function to add new notes to db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+//used for deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
+
+
+//HTML calls
+//calls home page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+//call for notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//Start listen
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
